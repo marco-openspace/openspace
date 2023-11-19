e3258b88cdcb6a3c7e1c509465d3daea231e6180
Marco - Openspace <149870435+marco-openspace@users.noreply.github.com>
2023-11-19 17:39:34 +0100

    +

--- components/openspace-home.tsx
+++ components/openspace-home.tsx
@@ -18,22 +18,43 @@ const OpenspaceHome = () => {
         disponibile in loco.
       </p>
       <p>Vieni a trovarci in Via Camillo Golgi 60, 20133 Milano!</p>
-      <div className="flex w-full justify-center py-8">
-        <Link
-          href="https://discord.gg/GqnrGPNW"
-          className="flex items-center rounded-lg border-4 border-[#7289da] px-4 py-2 hover:bg-[#7289da] hover:bg-opacity-40"
-        >
-          {" "}
-          <Image
-            className="mx-auto flex space-x-4 rounded-full border-white bg-[#7289da] p-3"
-            src={`${config.basePath}/assets/images/discord-logo.svg`}
-            alt="Discord Logo"
-            width={60}
-            height={60}
-            priority
-          />
-          <p className="ml-4 text-sm">Unisciti su Discord</p>
-        </Link>
+      <div className="md:flex md:py-4 md:justify-center">
+        <div className="flex w-full items-center flex-col space-y-0 md:space-y-0 md:max-w-[280px] md:flex-row justify-center pt-4 pb-2 space-x-4 px-4">
+          <Link
+            href="https://discord.gg/GqnrGPNW"
+            className="w-full flex items-center rounded-lg border-4 border-[#7289da] px-4 py-2 hover:bg-[#7289da] hover:bg-opacity-40"
+          >
+            {" "}
+            <Image
+              className="flex space-x-4 rounded-full border-white bg-[#7289da] p-3"
+              src={`${config.basePath}/assets/images/discord-logo.svg`}
+              alt="Discord Logo"
+              width={60}
+              height={60}
+              priority
+            />
+            <p className="ml-4 text-sm">Unisciti su Discord</p>
+          </Link>
+        </div>
+        <div className="flex w-full items-center flex-col space-y-0 md:space-y-0 md:max-w-[280px] md:flex-row justify-center pt-2 space-x-2 px-4">
+          <Link
+            href="https://form.jotform.com/carlonigiacomo/domanda-di-ammissione"
+            className="w-full flex items-center rounded-lg border-4 border-slate-400 py-2 hover:bg-slate-600 hover:bg-opacity-40"
+          >
+            {" "}
+            <Image
+              className="mx-1 flex space-x-0 rounded-lg border-white bg-black"
+              src={`${config.basePath}/assets/images/icon.png`}
+              alt="Jotform"
+              width={60}
+              height={60}
+              priority
+            />
+            <p className="ml-1 text-sm text-slate-300">
+              Unisciti all'associazione
+            </p>
+          </Link>
+        </div>
       </div>
     </div>
   );
