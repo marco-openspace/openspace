45a0a8b8a0ed21878792269affca339c6cbfc7e8
Marco - Openspace <149870435+marco-openspace@users.noreply.github.com>
2023-12-10 23:47:19 +0100

    membership

--- components/membership.tsx
+++ components/membership.tsx
@@ -7,12 +7,12 @@ const Membership = () => {
   return (
     <div className="flex flex-col w-full justify-center px-8 text-center text-slate-300 antialiased space-y-5">
       <h2 className="text-4xl font-bold">Membership</h2>
-      <p>
-        Chiunque è il benvenuto a diventare membro e contribuire alla comunità.
-        Ci sono tre livelli di adesione: Supporter, Core e Founder.
-      </p>
 
-      <div className="flex justify-center">
+      <div className="flex flex-col justify-center items-center">
+        <p className="max-w-4xl">
+          Chiunque è il benvenuto a diventare membro e contribuire alla
+          comunità. Ci sono tre livelli di adesione: Supporter, Core e Founder.
+        </p>
         <table className={`${borderStyles} max-w-4xl`}>
           <tbody className="">
             <tr className="text-3xl font-bold grid grid-cols-3">
@@ -50,11 +50,42 @@ const Membership = () => {
                 nominati al consiglio di amministrazione.
               </td>
             </tr>
+            <tr className="grid grid-cols-3 items-center">
+              <td className="border-2 border-b-0 border-white col-span-1 grid">
+                <Link
+                  href="https://forms.gle/asJs8yhPZ8E92veT8"
+                  className="col-span-1 cursor-pointer underline hover:bg-slate-500 hover:bg-opacity-40 p-3"
+                >
+                  Seleziona
+                </Link>
+              </td>
+              <td className="border-2 border-b-0 border-white col-span-1 grid">
+                <Link
+                  href="https://forms.gle/asJs8yhPZ8E92veT8"
+                  className="col-span-1 cursor-pointer underline hover:bg-yellow-500 hover:bg-opacity-40 p-3"
+                >
+                  Seleziona
+                </Link>
+              </td>
+              <td className="border-2 border-b-0 border-white col-span-1 grid">
+                <Link
+                  href="https://forms.gle/asJs8yhPZ8E92veT8"
+                  className="col-span-1 cursor-pointer underline hover:bg-teal-300 hover:bg-opacity-40 p-3"
+                >
+                  Seleziona
+                </Link>
+              </td>
+            </tr>
           </tbody>
         </table>
-      </div>
 
-      <p className="pt-4">ZZZZZZZ</p>
+        <p className="pt-4 max-w-4xl">
+          Stiamo ancora configurando il nostro sistema di pagamento. Se ti
+          iscrivi per un'adesione, puoi venire a trovarci di persona e pagare in
+          loco, oppure ti avviseremo quando saremo pronti ad accettare i
+          pagamenti.
+        </p>
+      </div>
       <div className="md:flex md:py-4 md:justify-center"></div>
     </div>
   );
